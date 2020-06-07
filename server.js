d6902b2dce9bc03da51d80675b60d7799df6640e
desuque <ldesuque@fi.uba.ar>
2020-06-07 11:38:27 +0200

    feat: core allow origin

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const PORT = process.env.PORT || 3000;
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
