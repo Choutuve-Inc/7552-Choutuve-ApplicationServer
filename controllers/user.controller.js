a99403a72fd67ae60f88b2b7c958aa1ed4ffda2a
desuque <ldesuque@fi.uba.ar>
2020-08-02 15:28:17 +0200

    feat: auth endpoints

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -53,13 +53,14 @@ exports.logout = (req, res) => {
 
     request.post('https://serene-shelf-10674.herokuapp.com/logout', {
         json: {
-            device: req.body.device
+            device: req.body.device,
+            email: req.body.email
         }
     }, (error, response, body) => {
         if (response.statusCode == 204) {
             deviceId = req.body.device
             console.log("Device a borrar:", deviceId)
-            
+
             const user = User.getUserByDevice(deviceId)
             if (user != undefined) {
                 User.delete(deviceId)
@@ -97,6 +98,162 @@ exports.create = (req, res) => {
             password: user.password,
             tipo: user.tipo,
             image: user.image
+        }
+    }, (error, response, body) => {
+        if (error) {
+            res.send(error)
+            return
+        }
+        res.send(body)
+    })
+};
+
+exports.token = (req, res) => {
+    if (!req.body) {
+        res.status(400).send({
+            message: req.body || "Content can not be empty!"
+        });
+    }
+
+    request.post('https://serene-shelf-10674.herokuapp.com/create', {
+        json: {
+            token: req.body.token
+        }
+    }, (error, response, body) => {
+        if (error) {
+            res.send(error)
+            return
+        }
+        res.send(body)
+    })
+};
+
+exports.getUsers = (req, res) => {
+    if (!req.body) {
+        res.status(400).send({
+            message: req.body || "Content can not be empty!"
+        });
+    }
+
+    request.get('https://serene-shelf-10674.herokuapp.com/users',
+        (error, response, body) => {
+            if (error) {
+                res.send(error)
+                return
+            }
+            res.send(body)
+        })
+};
+
+exports.getUser = (req, res) => {
+    if (!req.body) {
+        res.status(400).send({
+            message: req.body || "Content can not be empty!"
+        });
+    }
+
+    request.get('https://serene-shelf-10674.herokuapp.com/users/' + req.params.userId,
+        (error, response, body) => {
+            if (error) {
+                res.send(error)
+                return
+            }
+            res.send(body)
+        })
+};
+
+
+exports.updateUser = (req, res) => {
+    if (!req.body) {
+        res.status(400).send({
+            message: req.body || "Content can not be empty!"
+        });
+    }
+
+    const user = new User({
+        email: req.body.email,
+        phone: req.body.phone,
+        username: req.body.username,
+        password: req.body.password,
+        tipo: req.body.tipo,
+        image: req.body.image
+    });
+
+    request.patch('https://serene-shelf-10674.herokuapp.com/users/' + req.params.userId, {
+        json: {
+            email: user.email,
+            phone: user.phone,
+            username: user.username,
+            password: user.password,
+            tipo: user.tipo,
+            image: user.image
+        }
+    }, (error, response, body) => {
+        if (error) {
+            res.send(error)
+            return
+        }
+        res.send(body)
+    })
+};
+
+exports.deleteUser = (req, res) => {
+    if (!req.body) {
+        res.status(400).send({
+            message: req.body || "Content can not be empty!"
+        });
+    }
+
+    request.delete('https://serene-shelf-10674.herokuapp.com/users/' + req.params.userId,
+        (error, response, body) => {
+            if (error) {
+                res.send(error)
+                return
+            }
+            res.send(body)
+        })
+};
+
+exports.reset = (req, res) => {
+    if (!req.body) {
+        res.status(400).send({
+            message: req.body || "Content can not be empty!"
+        });
+    }
+
+    password = req.body.password
+    token = req.body.token
+    email = req.body.email
+    tipo = req.body.tipo
+
+    request.post('https://serene-shelf-10674.herokuapp.com/reset', {
+        json: {
+            password: password,
+            token: token,
+            email: email,
+            tipo: tipo,
+        }
+    }, (error, response, body) => {
+        if (error) {
+            res.send(error)
+            return
+        }
+        res.send(body)
+    })
+};
+
+exports.key = (req, res) => {
+    if (!req.body) {
+        res.status(400).send({
+            message: req.body || "Content can not be empty!"
+        });
+    }
+
+    email = req.body.email
+
+    request.post('https://serene-shelf-10674.herokuapp.com/key', {
+        json: {
+            email: email,
         }
     }, (error, response, body) => {
         if (error) {
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,6 +7,18 @@ module.exports = app => {
 
     app.post("/create", users.create)
 
-    // app.get("/users/:id", users.findUser)
+    app.post("/token", users.token)
+
+    app.get("/users", users.getUsers)
+
+    app.get("/users/:id", users.getUser)
+
+    app.patch("/users/:id", users.updateUser)
+
+    app.delete("/users/:id", users.deleteUser)
+
+    app.post("/reset", users.reset)
+
+    app.post("/key", users.key)
 
 };
